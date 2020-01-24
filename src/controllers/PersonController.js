f880b5ee431f350e4ab4e1f89304dd836ea0f79f
Antonio Cremonese - Tecnologia da Informação <antonio.cremonese@unimedsa.com.br>
2020-01-24 16:13:16 -0300

    adicionado ações no PersonController.js

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -9,17 +9,27 @@ exports.get = (req,res,next) =>{
 };
 exports.getById = (req,res,next) => {
     PersonRepository.getById(req.params.id)
-    .then()
-    .catch()
+    .then(person => {
+        res.status(200).send(person);
+    })
+    .catch(err => res.status(500).send(err))
 };
 exports.post = (req, res, next) => {
-    res.status(201).send('Requisição recebida com sucesso!');
+    const p = req.body;
+
+    PersonRepository.create(p)
+    .then(person => res.status(200).send(person))
+    .catch(err => res.status(500).send(err))
 };
 exports.put = (req, res, next) => {
-    let id = req.params.id;
-    res.status(201).send(`Requisição recebida com sucesso! ${id}`);
+    const p = req.body;
+
+    PersonRepository.update(req.params.id,p)
+    .then(person => res.status(201).send(person))
+    .catch(err => res.status(500).send(err))
 };
 exports.delete = (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send(`Requisição recebida com sucesso! ${id}`);
+    PersonRepository.delete(req.params.id)
+    .then(person => res.status(200).send('delete succeded!'))
+    .catch(err => res.status(500).err(err))
 };
